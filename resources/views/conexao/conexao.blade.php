78fff31083f06e7a6f61b71b403831ad3533b1d6
Vinicius <vinicius.fascina@saitro.com>
2020-11-27 12:01:40 -0300

    nada

--- resources/views/conexao/conexao.blade.php
+++ resources/views/conexao/conexao.blade.php
@@ -8,7 +8,7 @@
         <textarea id="textarea" name="textarea" placeholder="XML"></textarea>
         <div class="espaco">
             <input type="text" id="idProvisioning" name="idProvisioning" placeholder="ID Provisioning">
-            <button id="botao" type="button" class="btn btn-outline-danger">Enviar</button>
+            <button id="botao" type="submit" class="btn btn-outline-danger">Enviar</button>
         </div>
     </form>
 </div>
