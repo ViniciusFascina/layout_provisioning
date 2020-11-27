faf4fba4fc6b9b7c693832498e83b3ecca62af7d
Vinicius <vinicius.fascina@saitro.com>
2020-11-27 10:55:24 -0300

    implementacao de layouts e templates

---
+++ app/Http/Controllers/Conexao.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Services\Query;
+use App\Http\Services\XmlDecoder;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\SolicitacaoProvisioning;
+
+class Conexao extends Controller
+{
+    public function view(Request $request){
+        $id = $request->id_solicitacao;
+        $whatever = new Query();
+        return view('conexao/conexao',);
+    }
+
+    public function decode(Request $request)
+    {
+        $textarea = $_GET['textarea'];
+        $decode = new XmlDecoder();
+        $respCode = $decode->getRespcode($textarea);
+        $total = new Query();
+        $total = $total->resolve1001('724031218049725');
+        /*
+        passo2();
+        passo3();
+        passo4();
+        passo5();
+        passo6();
+        passo7();
+        */
+        return view('decode',compact('total'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Conexao;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/conexao/{id_solicitacao}', [Conexao::class,'whatever']);
+Route::get('/conexao', [Conexao::class,'view']);
+Route::get('/xmldecode', [Conexao::class,'decode']);
